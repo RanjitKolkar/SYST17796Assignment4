7f80a648141907ee3faafffab5fccbd04ef4a3da
Ranjit Kolkar <ranjit.kolkar@gmail.com>
2020-11-26 10:12:42 +0530

    Update PasswordValidator.java

--- src/passwordvalidator/PasswordValidator.java
+++ src/passwordvalidator/PasswordValidator.java
@@ -1,6 +1,6 @@
 package passwordvalidator;
 
-import java.util.Scanner;
+
 
 /**
  * A class to validate a user's chosen password. The password must have a length of at least 8 and at least one
@@ -10,39 +10,39 @@ import java.util.Scanner;
  * @author Haki Put your name and student is as modified by
  * @date
  */
-public class PasswordValidator {
 
-    /**
-     * Main method cycles through our static utility methods to determine whether the password is valid or not.
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        boolean isValid = false;
-
-        do {
-            System.out.println("Please enter a VALID password, password must have:");
+import java.util.regex.*; 
+import java.util.Scanner;
+public class ClassValidator
+{
+	public static void main(String[] args) {
+	    System.out.println("Please enter a VALID password, password must have:");
             System.out.println("A length of at least 8");
             System.out.println("At least one special character");
             System.out.println("At least one uppercase letter");
+            System.out.println("Please enter a password to be checked: ");
 
             Scanner sc = new Scanner(System.in);
             String password = sc.nextLine();
+            System.out.println(isValidPassword(password)); 
+	}
 
-            if (checkLength(password)) {
-                isValid = true;
-            }
-
-        } while (!isValid);
-
-        System.out.println("Valid password, accepted!");
-    }
-
-    public static boolean checkLength(String pass) {
-        
-        if (pass.length() >= 8) {
-            return true;
-        }
-        return false;
-    }
+    public static boolean
+    isValidPassword(String password) 
+    { 
+  
+        // Regex to check valid password. 
+        String pwd = "^(?=.*[0-9])"
+                       + "(?=.*[a-z])(?=.*[A-Z])"
+                       + "(?=.*[@#$%^&+=])"
+                       + "(?=\\S+$).{8,20}$"; 
+  
+        // Compile the ReGex 
+        Pattern p = Pattern.compile(pwd); 
+        if (password == null) { 
+            return false; 
+        }  
+        Matcher m = p.matcher(password); 
+        return m.matches(); 
+    } 
 }
